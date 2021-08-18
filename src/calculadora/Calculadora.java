b3f9365478f60a680c8a43877e92c859d101c065
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-17 20:07:34 -0400

    Initial commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+import javax.swing.*;
+
+public class Calculadora extends JFrame {
+
+
+    public Calculadora() {
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Calculadora();
+    }
+
+}

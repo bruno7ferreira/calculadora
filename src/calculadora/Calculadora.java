42e380ca8da99b20d31fd0e51c04097d0cdb6ea8
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-17 20:16:16 -0400

    Criando janela da Calculadora.java

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,11 +5,18 @@ import javax.swing.*;
 public class Calculadora extends JFrame {
 
 
+    //Construtor
     public Calculadora() {
 
-        setVisible(true);
+        setSize(232, 322); // definir tamanho da janela
+        setLocationRelativeTo(null); // para aplicação abrir centralizada na tela
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // Comando para fechar processo, quando a janela for fechada
+        setVisible(true); // deixar a janela visivel
+
     }
 
+
+    //Método MAIN
     public static void main(String[] args) {
         new Calculadora();
     }

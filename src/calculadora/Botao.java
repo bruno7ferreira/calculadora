67f5a1b420d826ea05297642344f5c2596450e84
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-19 16:19:46 -0400

    Configurando o projeto.

--- src/calculadora/Botao.java
+++ src/calculadora/Botao.java
@@ -7,9 +7,11 @@ public class Botao extends JButton {
 
     public Botao(String texto, Color cor) {
         setText(texto);
+        setFont(new Font("courier", Font.PLAIN, 25));
         setOpaque(true);
         setBackground(cor);
-        setFont(new Font("courier", Font.PLAIN, 25));
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
     }
 }

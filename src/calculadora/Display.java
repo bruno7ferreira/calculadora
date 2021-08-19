40480f88f3c5f20707fffb3c30dd7773a1d92181
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-19 00:25:21 -0400

    Configurando o projeto.

--- src/calculadora/Display.java
+++ src/calculadora/Display.java
@@ -1,11 +1,20 @@
 package calculadora;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Display extends JPanel {
 
-    public Display() {
+    private JLabel label;
 
+    public Display() {
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel("1234,56");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout());
+        add(label);
 
     }
 }
